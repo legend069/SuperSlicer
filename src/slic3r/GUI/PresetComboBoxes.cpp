2a1b2f11b8eae12ca8b216dfdbf1a6614158b07a
Stefan Weiss <stefan.weiss1@aol.com>
2024-04-10 22:31:11 +0200

    fixed tab selection

--- src/slic3r/GUI/PresetComboBoxes.cpp
+++ src/slic3r/GUI/PresetComboBoxes.cpp
@@ -653,13 +653,16 @@ void PlaterPresetComboBox::switch_to_tab()
         wxGetApp().tab_panel()->SetSelection(page_id);
         // Switch to Settings NotePad
         if (m_type == Preset::Type::TYPE_FFF_PRINT || m_type == Preset::Type::TYPE_SLA_PRINT)
-            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpPrintSettings);
+            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpPlaterGCode, true);
+
         else if (m_type == Preset::Type::TYPE_FFF_FILAMENT || m_type == Preset::Type::TYPE_SLA_MATERIAL)
-            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpFilamentSettings);
+            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpPlater, true);
+
         else if (m_type == Preset::Type::TYPE_PRINTER)
-            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpPrinterSettings);
+            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpPrinterSettings, true);
+
         else
-            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpLastSettings);
+            wxGetApp().mainframe->select_tab(MainFrame::TabPosition::tpLastSettings, false);
 
         //In a case of a multi-material printing, for editing another Filament Preset
         //it's needed to select this preset for the "Filament settings" Tab
