35b411028f60edbfacbc91ea0822e85e619677a7
legend069 <40685552+legend069@users.noreply.github.com>
2025-02-26 22:57:33 +1100

    fixed import related bug for jumping between SS versions.
    if a user tries to import a config file that has saved filament overrides
    SS will crash.
    
    will need to fix : FullPrintConfig::defaults(); so it generates default values for ALL config options?

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -320,10 +320,15 @@ void Preset::normalize(DynamicPrintConfig &config)
             if (key == "compatible_prints" || key == "compatible_printers")
                 continue;
             auto *opt = config.option(key, false);
+            auto *opt_default = defaults.option(key);
             /*assert(opt != nullptr);
             assert(opt->is_vector());*/
-            if (opt != nullptr && opt->is_vector())
-                static_cast<ConfigOptionVectorBase*>(opt)->resize(n, defaults.option(key));
+            if (opt != nullptr && opt->is_vector()){
+                if (opt_default != nullptr)
+                    static_cast<ConfigOptionVectorBase*>(opt)->resize(n, defaults.option(key));
+                //else
+                //    throw ConfigurationError("Invalid config option in filament_options: " + key);
+            }
         }
         // The following keys are mandatory for the UI, but they are not part of FullPrintConfig, therefore they are handled separately.
         for (const char *key : {"filament_settings_id"}) {
